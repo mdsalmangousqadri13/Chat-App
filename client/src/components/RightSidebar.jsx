78faaac6f6fd0c3213712434b7e7f944546cda1f
md salman gous qadri <145575717+mdsalmangousqadri13@users.noreply.github.com>
2025-06-27 18:15:44 +0530

    .

--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import assets from '../assets/assets'
 
-const RightSidebar = () => {
-  return (
+const RightSidebar = ({selectedUser}) => {
+  return selectedUser && (
     <div>
-        <h1>Right Sidebar</h1>
+        <div>
+          <img src={selectedUser ?.profilepic || assets.avatar_icon} alt="" className='w-20 aspect-[1/1] rounded-full' />
+          <h1 className='px-10 text-xl font-medium mx-auto flex items-cemter gap-2'>
+            <p className='w-2 h-2 rounded-full bg-green-500' ></p>
+            {selectedUser.fullName}
+          </h1>
+          <p className='px-10 mx-auto' >{selectedUser.bio}</p>
+        </div>
     </div>
   )
 }
 
 export default RightSidebar
+
