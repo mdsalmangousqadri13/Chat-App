f019f77122b6a758efb68c2f16903019abe4cd02
md salman gous qadri <145575717+mdsalmangousqadri13@users.noreply.github.com>
2025-06-27 22:31:40 +0530

    .

--- client/src/components/ChatCanainer.jsx
+++ client/src/components/ChatCanainer.jsx
@@ -51,7 +51,7 @@ const ChatCanainer = ({ selectedUser, setSelectedUser }) => {
     <div className='flex-1 flex itmes-center bg-gray-100/12 px-3 rounded-full'>
         <input type="text" placeholder="send a message" className='flex-1 text-sm p-3 border-none rounded-lg 
         outline-none text-white placeholder-gray-400'/>
-        <input type="file" id='image' accept='image/png, image/jpeg' hidden />
+        <input type="file" id='image' accept='image/png, image/jpeg,' hidden />
         <label htmlFor="image">
           <img src={assets.gallery_icon} alt="" className='w-5 mr-2 cursor-pointer' />
         </label>
