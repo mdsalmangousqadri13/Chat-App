c69b94f53ea5ca6c12e096877eaeeeb55457888e
md salman gous qadri <145575717+mdsalmangousqadri13@users.noreply.github.com>
2025-06-29 11:08:28 +0530

    push

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,9 +1,11 @@
 import Message from "../models/Message.js";
 import User from "../models/User.js";
+import cloudinary from "../lib/cloudinary.js";
+import { io, userSocketMap } from "../server.js";
 
 
 // Get all users except the logged in user
-export const getUserForSidebar = async (req, res)=>{
+export const getUsersForSidebar = async (req, res)=>{
     try {
         const userId = req.user._id;
         const filteredUsers = await User.find({_id: {$ne: userId}}).select("-password");
@@ -49,12 +51,46 @@ export const getMessages = async (req, res)=>{
 }
 
 
-//api to mark messafge as seen using message id
+//api to mark message as seen using message id
 export const markMessageAsSeen = async (req, res)=>{
     try {
         const { id } = req.params;
         await Message.findByIdAndUpdate(id, {seen: true})
         res.json({success: true})
+    } catch (error) {
+        console.log(error.messages);
+        res.json({success:false, messages: error.messages})
+    }
+}
+
+
+// Send message to selected users
+export const sendMessage = async (req, res)=>{
+    try {
+        const {text, image} = req.body;
+        const receiverId = req.params.id;
+        const senderId = req.user._id;
+
+        let imageUrl;
+        if(image){
+            const uploadResponse = await cloudinary.uploader.upload(image)
+            imageUrl = uploadResponse.secure_url;
+        }
+        const newMessage = await Message.create({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl
+        })
+
+        // Emit the new message to the receiver's socket
+        const receiverSocketId = userSocketMap[receiverId];
+        if(receiverSocketId){
+            io.to(receiverSocketId).emit("newMessage", newMessage)
+        }
+
+        res.json({success: true, newMessage});
+
     } catch (error) {
         console.log(error.messages);
         res.json({success:false, messages: error.messages})
--- server/server.js
+++ server/server.js
@@ -4,10 +4,39 @@ import cors from "cors";
 import http from "http";
 import { connectDB } from "./lib/db.js";
 import userRouter from "./routes/userRoutes.js";
+import messageRouter from "./routes/messageRoutes.js";
+import { Server } from "socket.io";
 
 // Create Express app and HTTP server
 const app = express();
 const server = http.createServer(app)  //http.createServer b/c socket.io support this server
+
+// Initialize socket.io server
+export const io = new Server(server, {
+    cors: {origin: "*"}
+})
+
+// Store online users
+export const userSocketMap = {};
+
+//Socket.io connection handler
+io.on("connection", (socket)=>{
+    const userId = socket.handshake.query.userId;
+    console.log("user Connected", userId);
+
+    if(userId) userSocketMap[userId] = socket.id;
+
+    //Emit online users to all connected clients
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+    socket.on("disconnect", ()=>{
+        console.log("/user Disconnected", userId);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap))
+        
+    })
+    
+})
 
 // Middleware setup
 app.use(express.json({limit: "4mb"}));
@@ -17,8 +46,9 @@ app.use(cors());
 //Routes setup
 app.use("/api/status", (req,res)=> res.send("server is live"));
 app.use("api/auth", userRouter);
+app.use("/api/messages", messageRouter)
 
-// COnnect to MongoDb
+// Connect to MongoDb
 await connectDB();
 
 const PORT = process.env.PORT || 5000;
